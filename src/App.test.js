0b8ace2ec26def705eecbda39aea133fa649802a
stelazlatinska <stela.zlatinska@fourth.com>
2019-01-04 16:47:33 +0200

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+* Factory function to create a shallow wrapper for the App component
+* @function setup
+* @param {object} pprops - Component props specific to this setup
+* @param {object} state - Initial state for setup
+* @returns {ShallowWrapper}
+*/
+const setup = (props={}, state=null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+
+  return wrapper;
+}
+
+/**
+* Return ShallowWrapper containing node(s) with the given data-test value
+* @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+* @param {String} val - Value of data-test attribute for search
+* @return {ShallowWrapper}
+*/
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
+
+it('renders without an error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+it('renders incement button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+it('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+it('counter starts at 0', () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+it('clicking button increments the counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  // find button and click
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+  wrapper.update();
+
+  //display and test value
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter+1);
 });
